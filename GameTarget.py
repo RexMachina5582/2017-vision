df3b70a2bd677c11910b2f78b69039b8a130df3b
RexMachina5582 <tcstechclub@gmail.com>
2017-01-21 11:55:37 -0500

    Initial commit

---
+++ GameTarget.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+class GameTarget:
+	def __init__(self):
+		targetType = None
+
+	def targetOffset(self):
+		# Take target pair and type, calculate center, and compare with
+		# desired location of target center for this target type.
+		# Return offset as positive or negative pixel count.
+		pass
+		
+	def targetType(self):
+		# Take a pair of target contours and determine if they are
+		# likely to be the boiler, the peg, or noise.
+		pass
+		
+	def targetPair(self):
+		# Define a mask and apply it to the current frame.
+		# Return the 2 largest countours matching the mask.
+		pass
+		
