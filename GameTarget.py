ea67c6ab301587e754a2e1bc5ca667226078afdb
Chris Taylor <beerobber@gmail.com>
2017-01-25 09:47:13 -0500

    GameTarget structured and pseudocoded

--- GameTarget.py
+++ GameTarget.py
@@ -4,19 +4,20 @@ import cv2
 
 class Patch:
     def __init__(self, countour):
-        M = cv2.moments(countour)
-        self.centerX = int(M["m10"] / M["m00"])
-        self.centerY = int(M["m01"] / M["m00"])
-        self.area = M["m00"]
+        # Reference our workshop code to find out how to use cv2.moments to calculate x, y, and area.
+        # Our trackTarget.py module is now in this project directory for easy reference.
+        self.centerX = 0
+        self.centerY = 0
+        self.area = 0
 
 class GameTarget:
 
-    # Boiler target patches have congruent X center points within this many pixels
-    X_CONGRUENCE_THRESHOLD = 5
+    # Boiler target patches have congruent X for center points within this many pixels
+    X_CONGRUENCE_THRESHOLD = 10
     # The larger boiler target patch is about double the area of the smaller
-    BOILER_TOP_TO_BOTTOM_RATIO = 0.5
+    BOILER_BOTTOM_TO_TOP_RATIO = 0.5
 
-    # Peg target patches have congruent Y center points within this many pixels
+    # Peg target patches have congruent Y for center points within this many pixels
     Y_CONGRUENCE_THRESHOLD = 15
     # Peg target patches are about equal in area
     PEG_LEFT_TO_RIGHT_RATIO = 1.0
@@ -53,17 +54,14 @@ class GameTarget:
         self.patchA = Patch(countourPair[0])
         self.patchB = Patch(countourPair[1])
 
-        # Patch A is the larger countour
-        patchRatio = self.patchA.area / self.patchB.area
+        # Patch A is the larger countour. Calculate its ratio, B as a % of A
+        patchRatio = 0.9
 
-        if abs(self.patchA.centerX - self.patchB.centerX) < GameTarget.X_CONGRUENCE_THRESHOLD \
-            and abs(GameTarget.BOILER_TOP_TO_BOTTOM_RATIO - patchRatio) < GameTarget.TARGET_RATIO_VARIANCE:
-            self.__confirmTarget("Boiler")
-            return True
-        elif abs(self.patchA.centerY - self.patchB.centerY) < GameTarget.Y_CONGRUENCE_THRESHOLD \
-            and abs(GameTarget.PEG_LEFT_TO_RIGHT_RATIO - patchRatio) < GameTarget.TARGET_RATIO_VARIANCE:
-            self.__confirmTarget("Peg")
-            return True
+        # if / elif statements. Does the absolute difference of patch X values fit within the X threshold?
+        # And does the absolute difference of theoretical and actual patch ratios fit within variance?
+        # If so, you've found the boiler target. You should confirm it. Then, "elif", do something very similar
+        # for the patch Y values and target ratio that would qualify a peg target. If you do confirm a target,
+        # make sure to return true before hitting the reset further down.
 
         self.__reset()
         return False
